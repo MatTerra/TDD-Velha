46727e60be264afcb5084bd9e13e6e0c19033199
MatTerra <mateus.b.s.terra@gmail.com>
2021-08-30 19:08:52 -0300

    Teste coluna

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -208,6 +208,23 @@ TEST_CASE("Testa coluna 1 toda 2 retorna 2",
     REQUIRE(VerificaVelha(teste1) == 2);
 }
 
+TEST_CASE("Testa coluna 2 toda igual retorna vencedor",
+          "[single-file]") {
+    int teste1[3][3] = {
+            {0, 1, 0},
+            {2, 1, 0},
+            {0, 1, 2}
+    };
+
+    REQUIRE(VerificaVelha(teste1) == 1);
+
+    int teste2[3][3] = {
+            {1, 0, 2},
+            {0, 1, 2},
+            {1, 0, 2}
+    };
+    REQUIRE(VerificaVelha(teste2) == 2);
+}
 // TEST_CASE("Testa velha", "[single-file]" ) {
 //   int teste1[3][3]= {
 //     { 2, 0, 1 },
